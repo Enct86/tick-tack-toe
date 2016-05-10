6ad6d0ce81ed27e8c11ba3127311826545b83d2c
Max Savchenko <enct86@gmail.com>
2016-05-10 14:08:52 +0300

    Add interface draw

--- src/tick/tack/toe/Game.java
+++ src/tick/tack/toe/Game.java
@@ -9,7 +9,24 @@ package tick.tack.toe;
  *
  * @author Max
  */
-public class Game {
-    private char gamefield[][] = new char[3][3];
-    
+public class Game implements Draw {
+
+    public char[][] gf = new char[3][3]; // Game Field
+    int gamenumber; // Game quantity for future)
+
+    public void draw() {
+        System.out.printf("%s %s %s\n%s %s %s\n%s %s %s\n", gf[0][0], gf[0][1], gf[0][2], gf[1][0], gf[1][1], gf[1][2], gf[2][0], gf[2][1], gf[2][2]);
+    }
+
+    public void clear() {
+        gf[0][0] = '-';
+        gf[0][1] = '-';
+        gf[0][2] = '-';
+        gf[1][0] = '-';
+        gf[1][1] = '-';
+        gf[1][2] = '-';
+        gf[2][0] = '-';
+        gf[2][1] = '-';
+        gf[2][2] = '-';
+    }
 }
--- src/tick/tack/toe/Main.java
+++ src/tick/tack/toe/Main.java
@@ -1,6 +1,6 @@
-
 package tick.tack.toe;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -9,21 +9,41 @@ import java.util.Scanner;
  */
 public class Main {
 
-
     public static void main(String[] args) {
         Scanner scanner1 = new Scanner(System.in);
         int i;
+        int k = 0;
+        boolean win = false;
         System.out.println("Добро пожаловать в игру Крестики Нолики");
-        do {  
+        do {
             System.out.printf("1 - Игра против AI, \n2 - Игра против друг друга\n3 \n4 - Выход\n");
             String input1 = scanner1.nextLine();
             i = Integer.parseInt(input1);
-            switch(i) {
+            switch (i) {
                 case 1:
-                    
+                    k++;
+                    Game g1 = new Game();
+                    g1.clear();
+                    g1.draw();
+                    do {
+                        System.out.println("Куда сделать ход (строка - ");
+                        String input2 = scanner1.nextLine();
+                        int stroka = Integer.parseInt(input2);
+                        System.out.println("Куда сделать ход (столбец - ");
+                        String input3 = scanner1.nextLine();
+                        int stolbec = Integer.parseInt(input3);
+                        if (g1.gf[stroka][stolbec] == '-') {
+                            g1.gf[stroka][stolbec] = 'X';
+                            g1.draw();
+                        } else {
+                            System.out.println("Ячейка занята");
+                        }
+                    } while (win == false);
             }
-            
-        } while (i != 4);
+            }
+            while (i != 4);
+
+        }
+        
     }
-    
-}
+
