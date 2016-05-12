c3f8fd8f484d264f8b82f74441880eaba9226687
Max Savchenko <enct86@gmail.com>
2016-05-12 13:21:39 +0300

    Add 2 turn AI mode

--- src/tick/tack/toe/Main.java
+++ src/tick/tack/toe/Main.java
@@ -12,9 +12,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner1 = new Scanner(System.in);
         int i;
-        int k = 0;
+        int pvaiturn = 0;
         int pvpturn = 0;
         boolean win = false;
+        int playerposition = 0;
         System.out.println("Добро пожаловать в игру Крестики Нолики");
         do {
             System.out.printf("1 - Игра против AI, \n2 - Игра против друг друга\n3 \n4 - Выход\n");
@@ -23,27 +24,128 @@ public class Main {
             switch (i) {
                 case 1:
                     win = false;
-                    k++;
                     Game g1 = new Game();
                     g1.clear();
                     g1.draw();
                     do {
-                        System.out.println("Куда сделать ход (строка - ");
-                        String input2 = scanner1.nextLine();
-                        int stroka = Integer.parseInt(input2);
-                        System.out.println("Куда сделать ход (столбец - ");
-                        String input3 = scanner1.nextLine();
-                        int stolbec = Integer.parseInt(input3);
-                        if (g1.gf[stroka][stolbec] == '-') {
-                            g1.gf[stroka][stolbec] = 'X';
-                            g1.draw();
-                        } else {
-                            System.out.println("Ячейка занята");
+                        boolean endturnplayer = false;
+                        do {
+                            System.out.println("Куда сделать ход (строка - ");
+                            String input2 = scanner1.nextLine();
+                            int stroka = Integer.parseInt(input2);
+                            System.out.println("Куда сделать ход (столбец - ");
+                            String input3 = scanner1.nextLine();
+                            int stolbec = Integer.parseInt(input3);
+                            if (g1.gf[stroka][stolbec] == '-') {
+                                g1.gf[stroka][stolbec] = 'X';
+                                endturnplayer = true;
+                                pvaiturn++;
+                            } else {
+                                System.out.println("Ячейка занята");
+                            }
+                        } while (endturnplayer == false);
+                        switch (pvaiturn) {
+                            case 1:
+                                if (g1.gf[1][1] == 'X') {
+                                    g1.gf[0][0] = 'O';
+                                    playerposition = 10;
+                                } else {
+                                    g1.gf[1][1] = 'O';
+                                    playerposition = 20;
+                                }
+                                g1.draw();
+                                break;
+                            case 2:
+                                switch (playerposition) {
+                                    case 10:
+                                        if (g1.gf[2][2] == 'X') {
+                                            g1.gf[2][0] = 'O';
+                                        }
+                                        if (g1.gf[0][1] == 'X') {
+                                            g1.gf[2][1] = 'O';
+                                        }
+                                        if (g1.gf[0][2] == 'X') {
+                                            g1.gf[2][0] = 'O';
+                                        }
+                                        if (g1.gf[1][2] == 'X') {
+                                            g1.gf[1][0] = 'O';
+                                        }
+                                        if (g1.gf[2][1] == 'X') {
+                                            g1.gf[0][1] = 'O';
+                                        }
+                                        if (g1.gf[2][0] == 'X') {
+                                            g1.gf[0][2] = 'O';
+                                        }
+                                        if (g1.gf[1][0] == 'X') {
+                                            g1.gf[1][2] = 'O';
+                                        }
+                                        g1.draw();
+                                        break;
+                                    case 20:
+                                        if (g1.gf[0][0] == 'X' & g1.gf[0][1] == 'X') {
+                                            g1.gf[0][2] = 'O';
+                                            playerposition = 21;
+                                        }
+                                        if (g1.gf[0][2] == 'X' & g1.gf[0][1] == 'X') {
+                                            g1.gf[0][0] = 'O';
+                                            playerposition = 22;
+                                        }
+                                        if (g1.gf[0][2] == 'X' & g1.gf[1][2] == 'X') {
+                                            g1.gf[2][2] = 'O';
+                                            playerposition = 23;
+                                        }
+                                        if (g1.gf[1][2] == 'X' & g1.gf[2][2] == 'X') {
+                                            g1.gf[0][2] = 'O';
+                                            playerposition = 24;
+                                        }
+                                        if (g1.gf[2][2] == 'X' & g1.gf[2][1] == 'X') {
+                                            g1.gf[2][0] = 'O';
+                                            playerposition = 25;
+                                        }
+                                        if (g1.gf[2][0] == 'X' & g1.gf[2][1] == 'X') {
+                                            g1.gf[2][2] = 'O';
+                                            playerposition = 26;
+                                        }
+                                        if (g1.gf[2][0] == 'X' & g1.gf[1][0] == 'X') {
+                                            g1.gf[0][0] = 'O';
+                                            playerposition = 27;
+                                        }
+                                        if (g1.gf[0][0] == 'X' & g1.gf[1][0] == 'X') {
+                                            g1.gf[2][0] = 'O';
+                                            playerposition = 28;
+                                        }
+                                        if (g1.gf[0][0] == 'X' & g1.gf[0][2] == 'X') {
+                                            g1.gf[0][1] = 'O';
+                                            playerposition = 29;
+                                        }
+                                        if (g1.gf[0][0] == 'X' & g1.gf[2][0] == 'X') {
+                                            g1.gf[1][0] = 'O';
+                                            playerposition = 32;
+                                        }
+                                        if (g1.gf[0][2] == 'X' & g1.gf[2][2] == 'X') {
+                                            g1.gf[1][2] = 'O';
+                                            playerposition = 30;
+                                        }
+                                        if (g1.gf[2][0] == 'X' & g1.gf[2][2] == 'X') {
+                                            g1.gf[2][1] = 'O';
+                                            playerposition = 31;
+                                        }
+                                        if (g1.gf[2][0] == 'X' & g1.gf[0][2] == 'X') {
+                                            g1.gf[0][1] = 'O';
+                                            playerposition = 33;
+                                        }
+                                        if (g1.gf[0][0] == 'X' & g1.gf[2][2] == 'X') {
+                                            g1.gf[0][1] = 'O';
+                                            playerposition = 34;
+                                        }
+                                        g1.draw();
+                                        break;
+                                }
+//                            case 3:
+
                         }
-                        g1.wincondX();
-                        win = g1.winconditionX;
                     } while (win == false);
-                    System.out.println("X WIN!");
+                    System.out.println("Drow !HUMAN CANT WIN ME!");
                     break;
                 case 2:
                     win = false;
@@ -98,9 +200,9 @@ public class Main {
 
                             g2.wincondO();
                             if (g2.winconditionO) {
-                                    System.out.println("O win!");
-                                    win = g2.winconditionO;
-                                }
+                                System.out.println("O win!");
+                                win = g2.winconditionO;
+                            }
                         }
                         if (pvpturn == 5 & win == false) {
                             win = true;
@@ -109,6 +211,7 @@ public class Main {
                     } while (win == false);
                     break;
             }
+
         } while (i != 4);
 
     }
